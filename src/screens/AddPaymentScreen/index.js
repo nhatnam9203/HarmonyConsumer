ff75adac1defdbb7058ec366324f3b88ff81c85f
Trần Hoàng Nhã <thediffest@gmail.com>
2022-07-08 12:19:27 +0700

    .

--- src/screens/AddPaymentScreen/index.js
+++ src/screens/AddPaymentScreen/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { View } from "react-native";
-import ScrollableTabView from "components/react-native-scrollable-tab-view";
+import React from 'react';
+import { View } from 'react-native';
+import ScrollableTabView from 'components/react-native-scrollable-tab-view';
 
-import * as RootNavigation from "navigations/RootNavigation";
-import { Header, Text, CreditTabBar, Form, StatusBar } from "components";
-import { BankForm, CreditForm } from "./widget";
-import { scaleSize } from "utils";
-import ICONS from "assets";
+import * as RootNavigation from 'navigations/RootNavigation';
+import { Header, Text, CreditTabBar, Form, StatusBar } from 'components';
+import { BankForm, CreditForm } from './widget';
+import { scaleSize } from 'utils';
+import ICONS from 'assets';
 
 export default function index() {
   const onBack = () => {
@@ -14,19 +14,19 @@ export default function index() {
   };
   const refTabCredit = React.useRef(null);
   return (
-    <View style={{ flex: 1, backgroundColor: "white" }}>
-      <View style={{ backgroundColor: "#f8f8f8" }}>
+    <View style={{ flex: 1, backgroundColor: 'white' }}>
+      <View style={{ backgroundColor: '#f8f8f8' }}>
         <StatusBar />
         <Header title="Add payments" headerLeft={true} onBack={onBack} />
       </View>
       <View
         style={{
-          alignSelf: "flex-start",
-          alignItems: "center",
+          alignSelf: 'flex-start',
+          alignItems: 'center',
           marginVertical: scaleSize(30),
           marginHorizontal: scaleSize(16),
         }}>
-        <Text fontSize={17} color="#888888" style={{ fontWeight: "500" }}>
+        <Text fontSize={17} color="#888888" style={{ fontWeight: '500' }}>
           Payment Methods
         </Text>
       </View>
@@ -35,9 +35,11 @@ export default function index() {
         <ScrollableTabView
           prerenderingSiblingsNumber={1}
           initialPage={0}
-          renderTabBar={() => <CreditTabBar style={{ marginLeft: scaleSize(16) }} />}>
-          <CreditForm tabLabel={ICONS["credit_form"]} ref={refTabCredit} />
-          <BankForm tabLabel={ICONS["bank_form"]} />
+          renderTabBar={() => (
+            <CreditTabBar style={{ marginLeft: scaleSize(16) }} />
+          )}>
+          <CreditForm tabLabel={ICONS['credit_form']} ref={refTabCredit} />
+          <BankForm tabLabel={ICONS['bank_form']} />
         </ScrollableTabView>
       </View>
       {/* </ScrollView> */}
