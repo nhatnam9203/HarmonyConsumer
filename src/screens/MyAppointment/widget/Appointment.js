8160b80513acd04761f847e0d977bf8416ed8928
Trần Hoàng Nhã <thediffest@gmail.com>
2022-10-06 11:11:21 +0700

    [HarmonyPay] make Unconfirm bigger
    https://harmonypay-team.atlassian.net/browse/POS1-743?atlOrigin=eyJpIjoiOTNlNTQ3MzdhMzU1NDc5MTgwN2NiZDA3MDc2ZWExY2UiLCJwIjoiaiJ9

--- src/screens/MyAppointment/widget/Appointment.js
+++ src/screens/MyAppointment/widget/Appointment.js
@@ -1,20 +1,28 @@
-import React from "react";
-import { View, StyleSheet, Platform, TouchableOpacity } from "react-native";
-import { scaleWidth, scaleHeight, textBottom, convertStatus } from "utils";
-import images from "assets";
-import moment from "moment";
-import { Text } from "components";
-import { useDispatch, useSelector } from "react-redux";
-import actions from "@redux/actions";
-import Image from "react-native-fast-image";
+import React from 'react';
+import { View, StyleSheet, Platform, TouchableOpacity } from 'react-native';
+import { scaleWidth, scaleHeight, textBottom, convertStatus } from 'utils';
+import images from 'assets';
+import moment from 'moment';
+import { Text } from 'components';
+import { useDispatch, useSelector } from 'react-redux';
+import actions from '@redux/actions';
+import Image from 'react-native-fast-image';
 
 export default function Appointment(props) {
-  const token = useSelector((state) => state.datalocalReducer.token);
+  const token = useSelector(state => state.datalocalReducer.token);
   const dispatch = useDispatch();
 
   const { appointment } = props;
-  const { fromTime, status, merchant, services, products, extras, appointmentId } = appointment;
-  const { businessName, address, merchantId } = merchant ? merchant : "";
+  const {
+    fromTime,
+    status,
+    merchant,
+    services,
+    products,
+    extras,
+    appointmentId,
+  } = appointment;
+  const { businessName, address, merchantId } = merchant ? merchant : '';
 
   const { banners = [] } = merchant ? merchant : [];
 
@@ -23,11 +31,13 @@ export default function Appointment(props) {
       ? { uri: banners[0].imageUrl, priority: Image.priority.high }
       : images.bannerMerchant;
 
-  const date = `${moment(fromTime).format("dddd,MMMM DD YYYY, hh:mm A")}`;
+  const date = `${moment(fromTime).format('dddd,MMMM DD YYYY, hh:mm A')}`;
 
   const viewAppointmentDetail = () => {
     props.viewDetailAppointment();
-    dispatch(actions.appointmentAction.getDetailAppointment(token, appointmentId));
+    dispatch(
+      actions.appointmentAction.getDetailAppointment(token, appointmentId),
+    );
     dispatch(actions.staffAction.staffGetByMerchant(merchantId, token));
   };
 
@@ -40,9 +50,15 @@ export default function Appointment(props) {
         <Text ellipsizeMode="tail" numberOfLines={1} style={styles.date}>
           {date}
         </Text>
-        <Info businessName={businessName} address={address} renderImgStore={renderImgStore} />
+        <Info
+          businessName={businessName}
+          address={address}
+          renderImgStore={renderImgStore}
+        />
         <View style={styles.bottom}>
-          <Text style={styles.txtBottom}>{textBottom(services, products, extras)}</Text>
+          <Text style={styles.txtBottom}>
+            {textBottom(services, products, extras)}
+          </Text>
           <Status status={status} />
         </View>
       </View>
@@ -52,30 +68,42 @@ export default function Appointment(props) {
 
 const Status = ({ status }) => {
   switch (status) {
-    case "unconfirm":
+    case 'unconfirm':
       return (
-        <View style={[styles.wrapStatus, { backgroundColor: "#FFFF80" }]}>
+        <View
+          style={[
+            styles.wrapStatus,
+            {
+              backgroundColor: '#FFFF20',
+              borderColor: 'white',
+              borderWidth: 1,
+            },
+          ]}>
           <Image source={images.icon_unconfirm} style={styles.iconStatus} />
-          <Text style={styles.txtStatus}>{convertStatus(status)}</Text>
+          <Text style={[styles.txtStatus, { color: '#444' }]}>
+            {convertStatus(status)}
+          </Text>
         </View>
       );
-    case "confirm":
+    case 'confirm':
       return (
-        <View style={[styles.wrapStatus, { backgroundColor: "#D4F8FC" }]}>
+        <View style={[styles.wrapStatus, { backgroundColor: '#D4F8FC' }]}>
           <Image source={images.icon_confirm} style={styles.iconStatus} />
           <Text style={styles.txtStatus}>{convertStatus(status)}</Text>
         </View>
       );
-    case "checkin":
+    case 'checkin':
       return (
-        <View style={[styles.wrapStatus, { backgroundColor: "#0764B0" }]}>
+        <View style={[styles.wrapStatus, { backgroundColor: '#0764B0' }]}>
           <Image source={images.icon_checkin} style={styles.iconStatus} />
-          <Text style={[styles.txtStatus, { color: "white" }]}>{convertStatus(status)}</Text>
+          <Text style={[styles.txtStatus, { color: 'white' }]}>
+            {convertStatus(status)}
+          </Text>
         </View>
       );
-    case "waiting":
+    case 'waiting':
       return (
-        <View style={[styles.wrapStatus, { backgroundColor: "#dddddd" }]}>
+        <View style={[styles.wrapStatus, { backgroundColor: '#dddddd' }]}>
           <Image source={images.icon_unconfirm} style={styles.iconStatus} />
           <Text style={[styles.txtStatus]}>{convertStatus(status)}</Text>
         </View>
@@ -97,7 +125,11 @@ const Info = ({ businessName, address, renderImgStore }) => {
           {businessName}
         </Text>
         <View style={styles.rowAddress}>
-          <Image source={images.geo_blue} style={styles.geo} tintColor="#666666" />
+          <Image
+            source={images.geo_blue}
+            style={styles.geo}
+            tintColor="#666666"
+          />
           <Text
             ellipsizeMode="tail"
             numberOfLines={1}
@@ -109,57 +141,56 @@ const Info = ({ businessName, address, renderImgStore }) => {
 };
 
 const styles = StyleSheet.create({
-  container: (status) => {
+  container: status => {
     return {
       width: scaleWidth(94),
-      borderRadius: 5,
-      borderWidth: 1,
+      borderRadius: 6,
+      borderWidth: status === 'unconfirm' ? 1.5 : 1,
       borderColor:
-        status === "unconfirm"
-          ? "#FFFF80"
-          : status === "confirm" || status === "waiting"
-          ? "#D4F8FC"
-          : "#0764B0",
+        status === 'unconfirm'
+          ? '#ffff60'
+          : status === 'confirm' || status === 'waiting'
+          ? '#D4F8FC'
+          : '#0764B0',
       padding: scaleWidth(5),
       marginTop: scaleHeight(2),
       marginLeft: scaleWidth(3),
-      backgroundColor: "white",
-      shadowColor: "#000",
+      backgroundColor: 'white',
+      shadowColor: '#000',
       shadowOffset: {
         width: 0,
         height: 2,
       },
       shadowOpacity: 0.15,
       shadowRadius: 3.84,
-
       elevation: 3,
     };
   },
   date: {
     fontSize: scaleWidth(4.2),
-    color: "#0764B0",
-    fontWeight: Platform.OS === "android" ? "bold" : "600",
+    color: '#0764B0',
+    fontWeight: Platform.OS === 'android' ? 'bold' : '600',
   },
   wrapInfo: {
     marginTop: scaleHeight(2),
-    flexDirection: "row",
+    flexDirection: 'row',
     borderBottomWidth: 1,
-    borderBottomColor: "#dddddd",
+    borderBottomColor: '#dddddd',
     paddingBottom: scaleWidth(3),
   },
   img: {
     width: scaleWidth(15),
     height: scaleWidth(15),
-    marginTop: Platform.OS === "android" ? scaleHeight(0.5) : 0,
+    marginTop: Platform.OS === 'android' ? scaleHeight(0.5) : 0,
     borderRadius: 5,
   },
   storeName: {
     fontSize: scaleWidth(4.5),
-    color: "#404040",
+    color: '#404040',
   },
   txtAddress: {
     fontSize: scaleWidth(3.8),
-    color: "#666666",
+    color: '#666666',
     width: scaleWidth(66),
   },
   geo: {
@@ -168,37 +199,49 @@ const styles = StyleSheet.create({
     marginRight: scaleWidth(1),
   },
   bottom: {
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     marginTop: scaleWidth(3),
   },
   right: {
     marginLeft: scaleWidth(3),
-    alignItems: "stretch",
+    alignItems: 'stretch',
   },
   txtBottom: {
     fontSize: scaleWidth(3.8),
-    color: "#404040",
+    color: '#404040',
   },
   rowAddress: {
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
     marginTop: scaleWidth(5),
   },
   wrapStatus: {
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
     paddingHorizontal: scaleWidth(3),
     paddingVertical: scaleWidth(1),
+    height: scaleWidth(12),
     borderRadius: 300,
   },
   txtStatus: {
-    fontSize: scaleWidth(3.5),
-    color: "#404040",
+    fontWeight: '500',
+    fontSize: scaleWidth(4.6),
+    color: '#444',
   },
   iconStatus: {
     width: scaleWidth(4),
     height: scaleWidth(4),
     marginRight: scaleWidth(1),
+  },
+  shadow: {
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.13,
+    shadowRadius: 2.82,
   },
 });
